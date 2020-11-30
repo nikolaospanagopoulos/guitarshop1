a992167e13280d1bf9ddc79dcb2e48da5e150a1c
NikosPanagopoulos <70824044+NikosPanagopoulos@users.noreply.github.com>
2020-12-01 01:51:43 +0200

    added new brands

--- frontend/src/screens/Brands.jsx
+++ frontend/src/screens/Brands.jsx
@@ -4,15 +4,15 @@ import {Card,Col,Row} from 'react-bootstrap'
 import dean from '../pictures/deanlogo.jpg'
 import fender from '../pictures/fenderlogo.jpg'
 import Ibanez from '../pictures/ibanezlogo.jpg'
-import jackson from '../pictures/jacksonlogo.jpg'
-import tama from '../pictures/tamalogo.png'
+import jackson from '../pictures/jackson.png'
+import tama from '../pictures/tamadrumslogo.jpg'
 const Brands = () => {
     return (
-        <Row xs={1} md={6} xl={2}>
+        <Row xs={12} md={6} xl={2}>
         <Col className='align-items-stretch d-flex' >
         <Card className="my-3 p-3 rounded" >
             <Link to={`/brand/dean`}>
-            <Card.Img src={dean} variant='top'/>
+            <Card.Img src={dean} variant='top' />
             </Link>
             <Card.Body>
             <Link to={`/brand/dean`}>
@@ -27,7 +27,7 @@ const Brands = () => {
         </Col>
 
 
-        <Col className='align-items-stretch d-flex'>
+        <Col className='align-items-stretch'>
         <Card className="my-3 p-3 rounded">
             <Link to={`/brand/Fender`}>
             <Card.Img src={fender} variant='top'/>
@@ -45,13 +45,13 @@ const Brands = () => {
         </Col>
 
 
-        <Col className='align-items-stretch d-flex'>
-        <Card className="my-3 p-3 rounded">
-            <Link to={`/brand/Ibanez`}>
-            <Card.Img src={Ibanez} variant='top'/>
+        <Col className='align-items-stretch d-flex '>
+        <Card className="my-3 p-3  rounded ">
+            <Link to={`/brand/ibanez`}>
+            <Card.Img src={Ibanez} variant='top'  />
             </Link>
             <Card.Body>
-            <Link to={`/brand/Ibanez`}>
+            <Link to={`/brand/ibanez`}>
             <Card.Title as='div'>
                 <strong>
                     <h2>Ibanez Guitars</h2> 
@@ -63,13 +63,14 @@ const Brands = () => {
         </Col>
 
 
+
         <Col className='align-items-stretch d-flex'>
-        <Card className="my-3 p-3 rounded">
-            <Link to={`/brand/Jackson`}>
-            <Card.Img src={jackson} variant='top'/>
+        <Card className="my-3 p-3  rounded">
+            <Link to={`/brand/jackson`}>
+            <Card.Img src={jackson} variant='top ' className='jackson'/>
             </Link>
             <Card.Body>
-            <Link to={`/brand/Jackson`}>
+            <Link to={`/brand/jackson`}>
             <Card.Title as='div'>
                 <strong>
                     <h2>Jackson Guitars</h2> 
@@ -78,14 +79,13 @@ const Brands = () => {
             </Link>
             </Card.Body>
         </Card>
-        
         </Col>
 
 
         <Col className='align-items-stretch d-flex'>
         <Card className="my-3 p-3 rounded">
             <Link to={`/brand/tama`}>
-            <Card.Img src={tama} variant='top'/>
+            <Card.Img src={tama} variant='top' />
             </Link>
             <Card.Body>
             <Link to={`/brand/tama`}>
